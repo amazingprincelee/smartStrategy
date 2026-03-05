88624836a21d51912f66967145606ec8eb5c2527
Prince Lee <prince_lee.nna85@ymail.com>
2026-03-05 12:28:40 +0100

    divine fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const authAPI = axios.create({
    baseURL: import.meta.env.VITE_API_URL        // production
- //  baseURL: import.meta.env.VITE_LOCAL_API_URL    // local dev
+  // baseURL: import.meta.env.VITE_LOCAL_API_URL    // local dev
 });
 
 // Request interceptor — attach Bearer token from localStorage
